96dbb8f4d78f90d7eed6b86ad41114019d5a5772
afnanmumu <afnanalauddin30@gmail.com>
2023-08-04 00:36:26 +0600

    raw nodejs project 4

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -9,6 +9,7 @@
 const data = require('../../lib/data');
 const { hash } = require('../../helpers/utilities');
 const { parseJSON } = require('../../helpers/utilities');
+const tokenHandler = require('./tokenHandler');
 
 // module scaffolding
 const handler = {};
@@ -98,18 +99,27 @@ handler._users.get = (requestProperties, callback) => {
             ? requestProperties.querystringobject.phone
             : false;
     if (phone) {
-        // lookup the user
-        data.read('users', phone, (err, u) => {
-            const user = { ...parseJSON(u) };
-            if (!err && user) {
-                delete user.password;
-                callback(200, user);
-            } else {
-                callback(404, {
-                    error: 'Requested user was not found!',
-                });
+         // verify token
+         const token =
+         typeof requestProperties.headersobject.token === 'string'
+             ? requestProperties.headersobject.token
+             : false;
+         tokenHandler._token.verify(token, phone, (tokenId) => {
+            if (tokenId) {
+                // lookup the user
+                data.read('users', phone, (err, u) => {
+                    const user = { ...parseJSON(u) };
+                    if (!err && user) {
+                        delete user.password;
+                        callback(200, user);
+                    } else {
+                        callback(404, {
+                            error: 'Requested user was not found!',
+                        });
+                    }
+                })
             }
-        });
+         });
     } else {
         callback(404, {
             error: 'Requested user was not found!',
@@ -146,52 +156,54 @@ handler._users.put = (requestProperties, callback) => {
 
     if (phone) {
         if (firstName || lastName || password) {
-            // loopkup the user
-            data.read('users', phone, (err1, uData) => {
-                const userData = { ...parseJSON(uData) };
-
-                if (!err1 && userData) {
-                    if (firstName) {
-                        userData.firstName = firstName;
-                    }
-                    if (lastName) {
-                        userData.lastName = lastName;
-                    }
-                    if (password) {
-                        userData.password = hash(password);
-                    }
-                    if (phone) {
-                        userData.phone = phone;
-                    }
-                    // store to database
-                    data.update('users', phone, userData, (err2) => {
-                        if (!err2) {
-                            callback(200, {
-                                message: `User was updated successfully! ${uData} ${firstName}`,
+            // verify token
+            const token =
+                typeof requestProperties.headersobject.token === 'string'
+                    ? requestProperties.headersobject.token
+                    : false;
+            tokenHandler._token.verify(token, phone, (tokenId) => {
+                        if (tokenId) {
+                        // loopkup the user
+                        data.read('users', phone, (err1, uData) => {
+                        const userData = { ...parseJSON(uData) };
+                        
+                        if (!err1 && userData) {
+                            if (firstName) {
+                                userData.firstName = firstName;
+                            }
+                            if (lastName) {
+                                userData.lastName = lastName;
+                            }
+                            if (password) {
+                                userData.password = hash(password);
+                            }
+                            // store to database
+                            data.update('users', phone, userData, (err2) => {
+                                if (!err2) {
+                                    callback(200, {
+                                        message: `User was updated successfully! ${uData} ${firstName}`,
+                                    });
+                                } else {
+                                    callback(500, {
+                                        error: 'There was a problem in the server side!',
+                                    });
+                                }
                             });
                         } else {
-                            callback(500, {
-                                error: 'There was a problem in the server side!',
+                            callback(400, {
+                                error: 'You have a problem in your request!',
                             });
                         }
                     });
-                } else {
-                    callback(400, {
-                        error: 'You have a problem in your request!',
-                    });
-                }
-            });
-        } else {
-            callback(400, {
-                error: 'You have a problem in your request!',
-            });
-        }
+                    } 
+                })
     } else {
         callback(400, {
             error: 'Invalid phone number. Please try again!',
         });
+        }
     }
-};
+}
 
 // @TODO: Authentication
 handler._users.delete = (requestProperties, callback) => {
---
+++ readme.md
@@ -0,0 +1,59 @@
+## Installation
+Nodejs
+Postman
+nodemon
+yarn(optional)
+
+## Run
+For staging - npm start
+For production - npm production
+
+## Task done
+Created a token system
+4 operation - post , get , put , delete
+Created a general purpose token verification
+Control all route access by token verification
+
+## User
+For post - http://localhost:3000/user
+{
+    "firstName" : "afnan" ,
+    "lastName" : "mumu" ,
+    "phone" : "01773046092" ,
+    "password" : "XXX" ,
+    "tosAgreement" : true
+}
+
+For get - http://localhost:3000/user?phone=01773046092
+Headers -> token = token_number
+
+For put - http://localhost:3000/user
+Headers -> token = token_number
+Phone number not mutable
+{
+    "firstName" : "afnan" ,
+    "lastName" : "alauddin" ,
+    "phone": "01773046092",
+    "password": "xxx"
+}
+
+For delete - http://localhost:3000/user?phone=01773046092
+Headers -> token = token_number
+
+## Token
+For post - http://localhost:3000/token
+{
+    "phone" : "01773046092" ,
+    "password" : "XXX" 
+}
+
+For get - http://localhost:3000/token?id=token_number
+
+For put - http://localhost:3000/token
+{
+    "id": token_number ,
+    "extend": true
+}
+
+For delete - http://localhost:3000/token?id=token_number
+
